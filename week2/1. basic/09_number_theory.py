856a96a3328b1ff672b3c7cfcc875fd1b458f463
sikhesung <hskang080326@gmail.com>
2026-08-30 14:55:16 +0900

    9번문제 해결

--- week2/1. basic/09_number_theory.py
+++ week2/1. basic/09_number_theory.py
@@ -113,7 +113,9 @@ def is_prime(n):
     """
     if n < 2 :
         return False
-    elif n % 2 == 0:
+    elif n % 2 == 1:
+        return True
+    elif n / 2 == 1:
         return True
     else:
         return False
