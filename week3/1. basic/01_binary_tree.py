c6eaa7b5e94b1456919f23e176da8617f8c2caae
sikhesung <hskang080326@gmail.com>
2026-09-03 21:53:09 +0900

    1번문제 해결

--- week3/1. basic/01_binary_tree.py
+++ week3/1. basic/01_binary_tree.py
@@ -44,15 +44,20 @@ def preorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
+    if root == None:
+        return result
     pass
     
     # TODO: 루트 값 추가
+    result.append(root.value)
     pass
     
     # TODO: 왼쪽 서브트리 순회
+    result.extend(preorder(root.left))
     pass
     
     # TODO: 오른쪽 서브트리 순회
+    result.extend(preorder(root.right))
     pass
     
     return result
@@ -62,15 +67,20 @@ def inorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
+    if root == None:
+        return result
     pass
     
     # TODO: 왼쪽 서브트리 순회
+    result.extend(inorder(root.left))
     pass
     
     # TODO: 루트 값 추가
+    result.append(root.value)
     pass
     
     # TODO: 오른쪽 서브트리 순회
+    result.extend(inorder(root.right))
     pass
     
     return result
@@ -80,15 +90,20 @@ def postorder(root):
     result = []
     
     # TODO: root가 None이면 빈 리스트 반환
+    if root == None:
+        return result
     pass
     
     # TODO: 왼쪽 서브트리 순회
+    result.extend(postorder(root.left))
     pass
     
     # TODO: 오른쪽 서브트리 순회
+    result.extend(postorder(root.right))
     pass
     
     # TODO: 루트 값 추가
+    result.append(root.value)
     pass
     
     return result
