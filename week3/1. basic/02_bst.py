ef4deea8fc4722d1422b2dc0777a3d1f88d06e77
sikhesung <hskang080326@gmail.com>
2026-09-04 08:55:52 +0900

    2번문제 해결

--- week3/1. basic/02_bst.py
+++ week3/1. basic/02_bst.py
@@ -51,11 +51,19 @@ def search_bst(root, target):
         True/False
     """
     # TODO: root가 None이면 False 반환
+    if root == None:
+        return False
     pass
-    
+
     # TODO: 값을 찾으면 True 반환
     ## target이 작으면 왼쪽 서브트리에서 검색
     ## target이 크면 오른쪽 서브트리에서 검색
+    if  root.value ==target:
+        return True
+    if target < root.value:
+        return search_bst(root.left , target)
+    else :
+        return search_bst(root.right , target)
     pass
 
 # 테스트 케이스
