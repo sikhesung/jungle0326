ffbbecda4b6ab53bebe14eca25be6cfdcd2a58ce
sikhesung <hskang080326@gmail.com>
2026-09-04 19:27:37 +0900

    7번문제 해결

--- week3/1. basic/07_dp_stairs.py
+++ week3/1. basic/07_dp_stairs.py
@@ -46,13 +46,24 @@ def climb_stairs(n):
         n번째 계단까지 오르는 방법의 수
     """
     # TODO: 특별한 경우 처리
+    if n == 0 :
+        return 0
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 2
     pass
     
     
     # TODO: dp 배열 생성 및 초기화
+    dp = [0] * (n + 1)
+    dp[1] = 1
+    dp[2] = 2
     pass
     
     # TODO: 작은 문제부터 차례로 계산
+    for i in range(3 , n+ 1):
+        dp[i] = dp[i-1] + dp[i-2]
     pass
     
     return dp[n]
