3f4752ad78ed2321b4c945ce6e0e865d066e2c3b
sikhesung <hskang080326@gmail.com>
2026-09-04 11:04:07 +0900

    3번문제 해결

--- week3/1. basic/03_graph_basic.py
+++ week3/1. basic/03_graph_basic.py
@@ -41,11 +41,16 @@ def create_graph(vertices, edges, directed=False):
         그래프 딕셔너리
     """
     # TODO: 빈 그래프 초기화
+    graph = {i: [] for i in range(vertices)}
     pass
     
     # TODO: 간선 추가
     ## 간선 추가 (u에서 v로)
     ## 무방향 그래프면 반대 방향도 추가
+    for u , v in edges:
+        graph[u].append(v)
+        if not directed:
+            graph[v].append(u)
     pass
     
     return graph
